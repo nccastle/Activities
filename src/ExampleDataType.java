f9f87ccc2605f3610a6903e52dcd461d40964cbf
nccastle <148673278+nccastle@users.noreply.github.com>
2023-11-12 17:55:56 +0800

    Add files via upload
    Example Data Type
    Project by: Noel Castillo

---
+++ src/ExampleDataType.java
@@ -0,0 +1,26 @@
+public class ExampleDataType {
+    public static void main(String[] args) {
+        System.out.println("Hello and Welcome");
+        System.out.println("Hi,how are you?");
+        System.out.println("I fine");
+
+        variables();
+    }
+
+    public static void variables() {
+        System.out.println("String : " + "Hello World!");
+        System.out.println("Integer : " + 123);
+        System.out.println("Double : " + 123.12);
+        System.out.println("Float : " + 123.32);
+        System.out.println("Boolean : " + true);
+        System.out.println("Character : " + 'A');
+
+        integer();
+    }
+    public static void integer() {
+        int x = 25;
+        System.out.println(x);
+
+
+    }
+}
